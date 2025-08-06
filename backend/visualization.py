eeea06a88f4b21ca6e4cac2dd430270002c7e70c
AntoinnetRjuan <ratsarafarajeanantoinnet@gmail.com>
2025-08-06 17:33:45 +0300

    ref: change the color of histplot

--- backend/visualization.py
+++ backend/visualization.py
@@ -3,7 +3,7 @@ import seaborn as sns
 
 def plot_histogram(df, column):
     plt.figure(figsize=(8, 5))
-    sns.histplot(df[column].dropna(), kde=True, color="#FF69B4")
+    sns.histplot(df[column].dropna(), kde=True, color="#1891EE")
     plt.title(f"Histogram of {column}", fontsize=14)
     plt.xlabel(column)
     plt.ylabel("Frequency")
